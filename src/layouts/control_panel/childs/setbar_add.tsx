cd82a0ad8d4229b05735f00ad6697078586a156f
asu <asueeer@163.com>
2022-08-13 00:20:17 +0800

    修改样式

--- src/layouts/control_panel/childs/setbar_add.tsx
+++ src/layouts/control_panel/childs/setbar_add.tsx
@@ -14,10 +14,12 @@ import {message} from "antd";
 
 interface Prop {
 }
-interface State{
-    adding:boolean
+
+interface State {
+    adding: boolean
 }
-export class SetBarAdd extends PureComponent<Prop,State> {
+
+export class SetBarAdd extends PureComponent<Prop, State> {
     constructor(props: any) {
         super(props)
     }
@@ -31,11 +33,12 @@ export class SetBarAdd extends PureComponent<Prop,State> {
     componentWillUnmount() {
         PaStateMan.unregist_comp(this)
     }
-    state={
-        adding:false
+
+    state = {
+        adding: false
     }
     rendercnt = 0;
-    newset_content=""
+    newset_content = ""
 
     render() {
         const seldir = PaStateMan.getstate().course_dir_sel_get()
@@ -68,56 +71,56 @@ export class SetBarAdd extends PureComponent<Prop,State> {
         this.rendercnt++;
 
         return (
-            this.state.adding?
-            <Box
-                sx={{
-                    marginRight:curstyle().gap.common,
-                    marginLeft:curstyle().gap.common,
-                }}
-            >
+            this.state.adding ?
+                <Box
+                    sx={{
+                        marginRight: curstyle().gap.common,
+                        marginLeft: curstyle().gap.common,
+                    }}
+                >
 
-                <Input1 placeholder={"输入新的分组名"}
-                        onLoad={(v:{focus:()=>void})=>{
-                            v.focus()
-                            console.log("onload",v)
-                        }}
-                    onChange={(e:any)=>{
-                        this.newset_content=e.target.value
-                    }}
-                        onBlur={()=>{
-                            if(this.newset_content!=""){
-                                PaStateMan.getstate().courseProxy().curcourse_add_new_dirs(
-                                    [this.newset_content]
-                                ).then((res)=>{
-                                    console.log(res)
-                                    if(res=="exist"){
-                                        message.warning('该分组已经存在！');
-                                    }
-                                    this.setState({
-                                        adding:false
+                    <Input1 placeholder={"输入新的分组名"}
+                            onLoad={(v: { focus: () => void }) => {
+                                v.focus()
+                                console.log("onload", v)
+                            }}
+                            onChange={(e: any) => {
+                                this.newset_content = e.target.value
+                            }}
+                            onBlur={() => {
+                                if (this.newset_content != "") {
+                                    PaStateMan.getstate().courseProxy().curcourse_add_new_dirs(
+                                        [this.newset_content]
+                                    ).then((res) => {
+                                        console.log(res)
+                                        if (res == "exist") {
+                                            message.warning('该分组已经存在！');
+                                        }
+                                        this.setState({
+                                            adding: false
+                                        })
                                     })
-                                })
-                            }else {
-                                this.setState({
-                                    adding:false
-                                })
-                            }
+                                } else {
+                                    this.setState({
+                                        adding: false
+                                    })
+                                }
+                            }}
+                    />
+                </Box> :
+                <Button className={
+                    reuse.trans_color_common + " "
+                    + reuse.row_flex2side_container
+                }
+                        onClick={() => {
+                            this.setState({
+                                adding: true
+                            }, () => {
+                            })
                         }}
-                />
-            </Box>:
-            <Button className={
-                reuse.trans_color_common + " "
-                + reuse.row_flex2side_container
-            }
-                    onClick={() => {
-                        this.setState({
-                            adding:true
-                        },()=>{
-                        })
-                    }}
-            >
-                + 新分组
-            </Button>
+                >
+                    + 新分组
+                </Button>
         )
     }
 }
