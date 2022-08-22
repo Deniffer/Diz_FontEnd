301968afd43f574c0b8c655831d5db62aa086417
pa <1020401660@qq.com>
2022-08-22 23:17:18 +0800

    tag样式

--- src/layouts/dir_select/dir_add_svg.tsx
+++ src/layouts/dir_select/dir_add_svg.tsx
@@ -3,8 +3,8 @@ import {Box} from "@mui/joy";
 
 export function add_dir_svg() {
     return (
-        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="32" height="32" rx="4" fill="#44B75E" fillOpacity="0.2"/>
+        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="34" height="34" rx="5" fill="#44B75E" fillOpacity="0.2"/>
             <path d="M14.9922 20.3537V13.6321H16.3295V20.3537H14.9922ZM12.3026 17.6591V16.3217H19.0241V17.6591H12.3026Z" fill="#44B75E"/>
         </svg>
     )
@@ -14,6 +14,7 @@ class DirAddSvg extends Component {
     render() {
         return (
             <Box sx={{
+                height:"min-content",
                 cursor: "pointer",
             }}>
                 {add_dir_svg()}
